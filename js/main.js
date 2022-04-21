756117882183d0819ddb59dda34f87d54bccafb8
dokkalemon <dottori.roberto@gmail.com>
2022-04-22 00:00:45 +0200

    add about-us section

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 /* References HTML */
 const navBar = document.querySelector(".navbar");
 const burger = document.querySelector(".burger");
+const slide = document.querySelectorAll(".slide");
 
 let activeNavBar = false;
+let activeSlide = 0;
 
 burger.addEventListener("click", function () {
   setActiveNavBar();
 });
 
 /* Function */
+//MENU MOBILE
 function setActiveNavBar() {
   if (activeNavBar === false) {
     activeNavBar = true;
@@ -25,5 +28,33 @@ function showNavBar() {
   } else {
     navBar.classList.remove("active");
     burger.classList.remove("active");
+  }
+}
+
+//CAROUSEL
+function increaseActiveSlide() {
+  activeSlide++;
+  if (activeSlide > 2) {
+    activeSlide = 0;
+  }
+  selectSlide();
+}
+
+function decreaseActiveSlide() {
+  activeSlide--;
+  if (activeSlide < 0) {
+    activeSlide = 2;
+  }
+  selectSlide();
+}
+
+function selectSlide() {
+  if (activeSlide === 1) {
+    slide[2].classList.add("active");
+  } else if (activeSlide === 2) {
+    slide[1].classList.add("active");
+  } else if (activeSlide === 0) {
+    slide[2].classList.remove("active");
+    slide[1].classList.remove("active");
   }
 }
