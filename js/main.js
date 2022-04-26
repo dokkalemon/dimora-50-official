7de4cf489ac2664ad07084a79e0c0d2de94aa4e3
dokkalemon <dottori.roberto@gmail.com>
2022-04-27 00:32:55 +0200

    add map and contact

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 const navBar = document.querySelector(".navbar");
 const burger = document.querySelector(".burger");
 const slide = document.querySelectorAll(".slide");
+const colazioni = document.getElementById("colazioni");
+const supermarket = document.getElementById("supermarket");
 
 let activeNavBar = false;
 let activeSlide = 0;
@@ -58,3 +60,65 @@ function selectSlide() {
     slide[1].classList.remove("active");
   }
 }
+
+function showLabel(checkbox, ref) {
+  checkbox.checked
+    ? ref.classList.add("active")
+    : ref.classList.remove("active");
+}
+
+/* DateRangePicker */
+
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, "0");
+var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+var yyyy = today.getFullYear();
+
+console.log(mm + "/" + dd + "/" + yyyy);
+$(function () {
+  $('input[name="datefilter"]').daterangepicker({
+    opens: "center",
+    autoUpdateInput: false,
+    autoApply: true,
+    minYear: 2022,
+    minDate: dd + "/" + mm + "/" + yyyy,
+
+    format: "MMMM D, YYYY",
+    locale: {
+      format: "MMMM D, YYYY",
+      daysOfWeek: ["Do", "Lu", "Ma", "Me", "Gi", "Ve", "Sa"],
+      monthNames: [
+        "Gennaio",
+        "Febbraio",
+        "Marzo",
+        "Aprile",
+        "Maggio",
+        "Giugno",
+        "Luglio",
+        "Agosto",
+        "Settembre",
+        "Ottobre",
+        "Novembre",
+        "Dicembre",
+      ],
+    },
+  });
+
+  $('input[name="datefilter"]').on(
+    "apply.daterangepicker",
+    function (ev, picker) {
+      $(this).val(
+        picker.startDate.format("DD/MM/YYYY") +
+          " - " +
+          picker.endDate.format("DD/MM/YYYY")
+      );
+    }
+  );
+
+  $('input[name="datefilter"]').on(
+    "cancel.daterangepicker",
+    function (ev, picker) {
+      $(this).val("");
+    }
+  );
+});
