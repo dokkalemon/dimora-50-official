cb891b2006d2a830373e5c5246ae456a9faa21bb
dokkalemon <dottori.roberto@gmail.com>
2022-04-29 22:16:41 +0200

    fix: :bug: carousel logic

--- js/main.js
+++ js/main.js
@@ -76,6 +76,7 @@ function increaseActiveSlide() {
   if (activeSlide > 2) {
     activeSlide = 0;
   }
+  console.log(activeSlide);
   selectSlide();
   constolSlideHandler(activeSlide);
 }
@@ -85,6 +86,7 @@ function decreaseActiveSlide() {
   if (activeSlide < 0) {
     activeSlide = 2;
   }
+  console.log(activeSlide);
   selectSlide();
   constolSlideHandler(activeSlide);
 }
@@ -93,11 +95,9 @@ function selectSlide() {
   const slide = document.querySelectorAll(".slide");
   for (let i = 0; i < slide.length; i++) {
     if (i === activeSlide) {
-      slide[i].classList.remove("active");
-      console.log(i);
-    } else {
       slide[i].classList.add("active");
-      console.log(i);
+    } else {
+      slide[i].classList.remove("active");
     }
   }
 }
@@ -211,7 +211,6 @@ function controlSlide(index) {
   const slidePreview = document.querySelectorAll(".slide-preview");
 
   galleryActiveSlide = index;
-  console.log(galleryActiveSlide);
   changePreview();
   changeSlide();
 }
@@ -228,7 +227,6 @@ var dd = String(today.getDate()).padStart(2, "0");
 var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
 var yyyy = today.getFullYear();
 
-console.log(mm + "/" + dd + "/" + yyyy);
 $(function () {
   $('input[name="datefilter"]').daterangepicker({
     opens: "center",
@@ -268,11 +266,160 @@ $(function () {
       );
     }
   );
+});
 
-  $('input[name="datefilter"]').on(
-    "cancel.daterangepicker",
-    function (ev, picker) {
-      $(this).val("");
+/* COOKIE */
+
+//<![CDATA[
+(function (window) {
+  if (!!window.cookieChoices) {
+    return window.cookieChoices;
+  }
+  var document = window.document;
+  var supportsTextContent = "textContent" in document.body;
+  var cookieChoices = (function () {
+    var cookieName = "displayCookieConsent";
+    var cookieConsentId = "cookieChoiceInfo";
+    var dismissLinkId = "cookieChoiceDismiss";
+    function _createHeaderElement(cookieText, dismissText, linkText, linkHref) {
+      var butterBarStyles =
+        "position:fixed;width:100%; font-size: 20px;height: 9%; display: flex; align-items: center; justify-content: center;background-color:#111111;color:#151515" +
+        "margin:0; left:0; bottom:0;padding:4px;z-index:1000;text-align:center;font-family:'Josefin Sans'";
+      var cookieConsentElement = document.createElement("div");
+      cookieConsentElement.id = cookieConsentId;
+      cookieConsentElement.style.cssText = butterBarStyles;
+      cookieConsentElement.appendChild(_createConsentText(cookieText));
+      if (!!linkText && !!linkHref) {
+        cookieConsentElement.appendChild(
+          _createInformationLink(linkText, linkHref)
+        );
+      }
+      cookieConsentElement.appendChild(_createDismissLink(dismissText));
+      return cookieConsentElement;
     }
+    function _createDialogElement(cookieText, dismissText, linkText, linkHref) {
+      var glassStyle =
+        "position:fixed;width:100%;height:100%;z-index:999;" +
+        "bottom:0;left:0;opacity:0.5;filter:alpha(opacity=50);" +
+        "background-color:#000000;";
+      var dialogStyle = "z-index:1000;position:fixed;left:50%;top:50%,";
+      var contentStyle =
+        "position:relative;left:-50%;margin-top:-25%;" +
+        "background-color:#000000;padding:20px;box-shadow:4px 4px 25px #888;";
+      var cookieConsentElement = document.createElement("div");
+      cookieConsentElement.id = cookieConsentId;
+      var glassPanel = document.createElement("div");
+      glassPanel.style.cssText = glassStyle;
+      var content = document.createElement("div");
+      content.style.cssText = contentStyle;
+      var dialog = document.createElement("div");
+      dialog.style.cssText = dialogStyle;
+      var dismissLink = _createDismissLink(dismissText);
+      dismissLink.style.display = "block";
+      dismissLink.style.textAlign = "right";
+      dismissLink.style.marginTop = "8px";
+      content.appendChild(_createConsentText(cookieText));
+      if (!!linkText && !!linkHref) {
+        content.appendChild(_createInformationLink(linkText, linkHref));
+      }
+      content.appendChild(dismissLink);
+      dialog.appendChild(content);
+      cookieConsentElement.appendChild(glassPanel);
+      cookieConsentElement.appendChild(dialog);
+      return cookieConsentElement;
+    }
+    function _setElementText(element, text) {
+      if (supportsTextContent) {
+        element.textContent = text;
+      } else {
+        element.innerText = text;
+      }
+    }
+    function _createConsentText(cookieText) {
+      var consentText = document.createElement("span");
+      _setElementText(consentText, cookieText);
+      return consentText;
+    }
+    function _createDismissLink(dismissText) {
+      var dismissLink = document.createElement("a");
+      _setElementText(dismissLink, dismissText);
+      dismissLink.id = dismissLinkId;
+      dismissLink.href = "#";
+      dismissLink.style.marginLeft = "24px";
+      return dismissLink;
+    }
+    function _createInformationLink(linkText, linkHref) {
+      var infoLink = document.createElement("a");
+      _setElementText(infoLink, linkText);
+      infoLink.href = linkHref;
+      infoLink.target = "_blank";
+      infoLink.style.marginLeft = "8px";
+      return infoLink;
+    }
+    function _dismissLinkClick() {
+      _saveUserPreference();
+      _removeCookieConsent();
+      return false;
+    }
+    function _showCookieConsent(
+      cookieText,
+      dismissText,
+      linkText,
+      linkHref,
+      isDialog
+    ) {
+      if (_shouldDisplayConsent()) {
+        _removeCookieConsent();
+        var consentElement = isDialog
+          ? _createDialogElement(cookieText, dismissText, linkText, linkHref)
+          : _createHeaderElement(cookieText, dismissText, linkText, linkHref);
+        var fragment = document.createDocumentFragment();
+        fragment.appendChild(consentElement);
+        document.body.appendChild(fragment.cloneNode(true));
+        document.getElementById(dismissLinkId).onclick = _dismissLinkClick;
+      }
+    }
+    function showCookieConsentBar(cookieText, dismissText, linkText, linkHref) {
+      _showCookieConsent(cookieText, dismissText, linkText, linkHref, false);
+    }
+    function showCookieConsentDialog(
+      cookieText,
+      dismissText,
+      linkText,
+      linkHref
+    ) {
+      _showCookieConsent(cookieText, dismissText, linkText, linkHref, true);
+    }
+    function _removeCookieConsent() {
+      var cookieChoiceElement = document.getElementById(cookieConsentId);
+      if (cookieChoiceElement != null) {
+        cookieChoiceElement.parentNode.removeChild(cookieChoiceElement);
+      }
+    }
+    function _saveUserPreference() {
+      // Durata del cookie di un anno
+      var expiryDate = new Date();
+      expiryDate.setFullYear(expiryDate.getFullYear() + 1);
+      document.cookie = cookieName + "=y; expires=" + expiryDate.toGMTString();
+    }
+    function _shouldDisplayConsent() {
+      // Per mostrare il banner solo in mancanza del cookie
+      return !document.cookie.match(new RegExp(cookieName + "=([^;]+)"));
+    }
+    var exports = {};
+    exports.showCookieConsentBar = showCookieConsentBar;
+    exports.showCookieConsentDialog = showCookieConsentDialog;
+    return exports;
+  })();
+  window.cookieChoices = cookieChoices;
+  return cookieChoices;
+})(this);
+document.addEventListener("DOMContentLoaded", function (event) {
+  cookieChoices.showCookieConsentBar(
+    "Questo sito utilizza i cookie per migliorare servizi ed esperienza dei lettori. Se decidi di continuare la navigazione consideriamo che accetti il loro uso.",
+    "OK",
+    "Info",
+    "cookie-policy.html"
   );
 });
+//]]>
